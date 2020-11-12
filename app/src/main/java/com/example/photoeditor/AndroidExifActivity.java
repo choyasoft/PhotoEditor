af68af9710012f619c8ce997c044a7edeab1954d
IvySoft <tarraconeros@gmail.com>
2020-11-12 15:57:26 +0100

    Mejoras en la implementación de Maps. Falta traducir coordenadas

--- app/src/main/java/com/example/photoeditor/AndroidExifActivity.java
+++ app/src/main/java/com/example/photoeditor/AndroidExifActivity.java
@@ -46,19 +46,6 @@ public class AndroidExifActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_exif);
 
-      /*  Button btnMostrarMapa = (Button) findViewById(R.id.btnMostrarMapa);
-        btnMostrarMapa.setOnClickListener(new View.OnClickListener() {
-                                              @Override
-                                              public void onClick(View view) {
-
-                                                  Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                                                          Uri.parse("geo:0,0?q="+TAG_GPS_LATITUDE+", "+TAG_GPS_LONGITUDE));
-                                                  intent.setPackage("com.google.android.apps.maps");
-                                                  startActivity(intent);
-
-                                              }
-                                          });
-*/
         image = (ImageView) findViewById(R.id.image);
         Exif = (TextView)findViewById(R.id.exif);
         ImageView image = (ImageView)findViewById(R.id.image);
@@ -75,33 +62,21 @@ public class AndroidExifActivity extends Activity {
         try {
             ExifInterface exifInterface = new ExifInterface(file);
 
-           /* exif += "\nIMAGE_LENGTH: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_LENGTH);
-            exif += "\nIMAGE_WIDTH: " + exifInterface.getAttribute(ExifInterface.TAG_IMAGE_WIDTH);
-            exif += "\n DATETIME: " + exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
-            exif += "\n TAG_MAKE: " + exifInterface.getAttribute(ExifInterface.TAG_MAKE);
-            exif += "\n TAG_MODEL: " + exifInterface.getAttribute(ExifInterface.TAG_MODEL);
-            exif += "\n TAG_ORIENTATION: " + exifInterface.getAttribute(ExifInterface.TAG_ORIENTATION);
-            exif += "\n TAG_WHITE_BALANCE: " + exifInterface.getAttribute(ExifInterface.TAG_WHITE_BALANCE);
-            exif += "\n TAG_FOCAL_LENGTH: " + exifInterface.getAttribute(ExifInterface.TAG_FOCAL_LENGTH);
-            exif += "\n TAG_FLASH: " + exifInterface.getAttribute(ExifInterface.TAG_FLASH);
-            */
-
             exif += "\nDatos de ubicación GPS:";
-          //     exif += "\n TAG_GPS_DATESTAMP: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_DATESTAMP);
-          //     exif += "\n TAG_GPS_TIMESTAMP: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_TIMESTAMP);
+
             exif += "\n Latitud: " + exifInterface.getAttribute(TAG_GPS_LATITUDE);
             String lat1 = exifInterface.getAttribute(TAG_GPS_LATITUDE);
               // exif += "\n Latitud REF: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LATITUDE_REF);
             exif += "\n Longitud: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE);
             String lon1 = exifInterface.getAttribute(TAG_GPS_LATITUDE);
            //    exif += "\n TAG_GPS_LONGITUDE_REF: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_LONGITUDE_REF);
-          //     exif += "\n TAG_GPS_PROCESSING_METHOD: " + exifInterface.getAttribute(ExifInterface.TAG_GPS_PROCESSING_METHOD);
 
-            // FUA
-          //double latDouble = Double.parseDouble(lat1);
-          // double lonDouble = Double.parseDouble(lon1);
 
-           // System.out.println(geoloc);
+           /*
+          double latDouble = Double.parseDouble(lat1);
+          double lonDouble = Double.parseDouble(lon1);
+          LatLng latlng = new LatLng(Double.parseDouble(lat1),Double.parseDouble(lon1));
+        */
             Toast.makeText(this, lat1.replaceAll("[^,.\\dA-Za-z0-9]", " "), Toast.LENGTH_LONG).show();
             Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
                     Uri.parse("geo:0,0?q="+ lat1.replaceAll("[^,.\\dA-Za-z0-9]", " ")+lon1.replaceAll("[^,.\\dA-Za-z0-9]", " ")));
